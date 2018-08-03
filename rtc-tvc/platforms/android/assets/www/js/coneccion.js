6bf0f1672f3f70f42123c03310095058ede812c0
Jgaleas7 <juangaleassantos@gmail.com>
2018-08-02 19:08:26 -0600

    primer comit

---
+++ rtc-tvc/platforms/android/assets/www/js/coneccion.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyCSzc6CLeilhDVsrxbCEAjVLFOL8ZWPuC0",
+    authDomain: "what-tv.firebaseapp.com",
+    databaseURL: "https://what-tv.firebaseio.com",
+    projectId: "what-tv",
+    storageBucket: "what-tv.appspot.com",
+    messagingSenderId: "885628955414"
+};
+firebase.initializeApp(config);
